702d978abc666aa3779f66dd2eb06556f73737df
ApMatheus <matheusaparecido101@gmail.com>
2023-04-29 19:05:35 -0300

    feat(Features) styles features

--- components/ui/Features.tsx
+++ components/ui/Features.tsx
@@ -25,20 +25,20 @@ function FeatureHighlights(
   { features }: Props,
 ) {
   return (
-    <div class="container min-h-[280px] p-6 sm:px-0 sm:py-10">
-      <div class="border-base-200 border border-solid">
+    <div class="container min-h-[220px] sm:px-0 sm:pt-10 bg-white">
+      <div class="border-base-200 border-t">
         <div class="flex flex-col justify-evenly divide-y divide-base-200 mx-6 sm:flex-row sm:divide-y-0 sm:divide-x sm:mx-0 sm:my-10">
           {features.map(({ icon: id = "Truck", title, description }) => (
-            <div class="flex flex-row gap-4 py-6 sm:flex-col sm:py-0 sm:px-8">
+            <div class="flex flex-row gap-4 py-6 sm:flex-col sm:py-0 sm:px-8 sm:items-center">
               <Icon
                 id={id}
                 width={40}
                 height={40}
                 strokeWidth={2}
               />
-              <div class="flex flex-col gap-2">
-                <span class="font-medium text-xl">{title}</span>
-                <span class="text-sm">{description}</span>
+              <div class="flex flex-col w-full gap-1">
+                <span class="font-medium text-base font-firaSans text-center text-text-color-secord">{title}</span>
+                <span class="text-xs text-center font-firaSans text-text-color-secord">{description}</span>
               </div>
             </div>
           ))}
