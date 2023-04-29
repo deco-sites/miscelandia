4b8d9007f679dfbd5ba99410c2ca20ae91863ac5
ApMatheus <matheusaparecido101@gmail.com>
2023-04-29 18:34:06 -0300

    feat(TextInfo) created TextInfo

--- sections/TextInfo.tsx
+++ sections/TextInfo.tsx
@@ -1,2 +1,2 @@
-export type {Props} from "../components/ui/TextInfo.tsx"
-export {default} from "../components/ui/TextInfo.tsx"
+export type { Props } from "../components/ui/TextInfo.tsx";
+export { default } from "../components/ui/TextInfo.tsx";
