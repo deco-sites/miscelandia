2864d0fa3728063aa12fda25c0bc33937309794b
ApMatheus <matheusaparecido101@gmail.com>
2023-04-29 18:04:07 -0300

    feat(Banner New Collections) created Banner in new collections

--- sections/BannerNewCollections.tsx
+++ sections/BannerNewCollections.tsx
@@ -1,3 +1,3 @@
 export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
-export type {Image, Props} from "../components/ui/BannerNewCollections.tsx";
-export {default} from "../components/ui/BannerNewCollections.tsx";
+export type { Image, Props } from "../components/ui/BannerNewCollections.tsx";
+export { default } from "../components/ui/BannerNewCollections.tsx";
